e985e8be4c6de60c08d1ee84157f0392883261cd
Joses Kang <joseskang@gmail.com>
2021-02-05 21:38:52 +0800

    Cleaned up main page and files

---
+++ inc/general/sidebar.php
@@ -0,0 +1,226 @@
+<div class="app-sidebar sidebar-shadow">
+    <div class="app-header__logo">
+        <div class="logo-src"></div>
+        <div class="header__pane ml-auto">
+            <div>
+                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="app-header__mobile-menu">
+        <div>
+            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
+            </button>
+        </div>
+    </div>
+    <div class="app-header__menu">
+        <span>
+            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                <span class="btn-icon-wrapper">
+                    <i class="fa fa-ellipsis-v fa-w-6"></i>
+                </span>
+            </button>
+        </span>
+    </div>
+
+    <div class="scrollbar-sidebar">
+        <div class="app-sidebar__inner">
+            <ul class="vertical-nav-menu">
+                <li class="app-sidebar__heading">Dashboards</li>
+                <li>
+                    <a href="index.php" class="mm-active">
+                        <i class="metismenu-icon pe-7s-rocket"></i>
+                        Dashboard Example 1
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">UI Components</li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-diamond"></i>
+                        Elements
+                        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="php/elements-buttons-standard.php">
+                                <i class="metismenu-icon"></i>
+                                Buttons
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/elements-dropdowns.php">
+                                <i class="metismenu-icon">
+                                </i>Dropdowns
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/elements-icons.php">
+                                <i class="metismenu-icon">
+                                </i>Icons
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/elements-badges-labels.php">
+                                <i class="metismenu-icon">
+                                </i>Badges
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/elements-cards.php">
+                                <i class="metismenu-icon">
+                                </i>Cards
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/elements-list-group.php">
+                                <i class="metismenu-icon">
+                                </i>List Groups
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/elements-navigation.php">
+                                <i class="metismenu-icon">
+                                </i>Navigation Menus
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/elements-utilities.php">
+                                <i class="metismenu-icon">
+                                </i>Utilities
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="metismenu-icon pe-7s-car"></i>
+                        Components
+                        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="php/components-tabs.php">
+                                <i class="metismenu-icon">
+                                </i>Tabs
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-accordions.php">
+                                <i class="metismenu-icon">
+                                </i>Accordions
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-notifications.php">
+                                <i class="metismenu-icon">
+                                </i>Notifications
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-modals.php">
+                                <i class="metismenu-icon">
+                                </i>Modals
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-progress-bar.php">
+                                <i class="metismenu-icon">
+                                </i>Progress Bar
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-tooltips-popovers.php">
+                                <i class="metismenu-icon">
+                                </i>Tooltips &amp; Popovers
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-carousel.php">
+                                <i class="metismenu-icon">
+                                </i>Carousel
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-calendar.php">
+                                <i class="metismenu-icon">
+                                </i>Calendar
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-pagination.php">
+                                <i class="metismenu-icon">
+                                </i>Pagination
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-scrollable-elements.php">
+                                <i class="metismenu-icon">
+                                </i>Scrollable
+                            </a>
+                        </li>
+                        <li>
+                            <a href="php/components-maps.php">
+                                <i class="metismenu-icon">
+                                </i>Maps
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="php/tables-regular.php">
+                        <i class="metismenu-icon pe-7s-display2"></i>
+                        Tables
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Widgets</li>
+                <li>
+                    <a href="php/dashboard-boxes.php">
+                        <i class="metismenu-icon pe-7s-display2"></i>
+                        Dashboard Boxes
+                    </a>
+                </li>
+                <li class="app-sidebar__heading">Forms</li>
+                <li>
+                    <a href="php/forms-controls.php">
+                        <i class="metismenu-icon pe-7s-mouse">
+                        </i>Forms Controls
+                    </a>
+                </li>
+                <li>
+                    <a href="php/forms-layouts.php">
+                        <i class="metismenu-icon pe-7s-eyedropper">
+                        </i>Forms Layouts
+                    </a>
+                </li>
+                <li>
+                    <a href="php/forms-validation.php">
+                        <i class="metismenu-icon pe-7s-pendrive">
+                        </i>Forms Validation
+                    </a>
+                </li>
+                <li class="php/app-sidebar__heading">Charts</li>
+                <li>
+                    <a href="charts-chartjs.php">
+                        <i class="metismenu-icon pe-7s-graph2">
+                        </i>ChartJS
+                    </a>
+                </li>
+                <li class="php/app-sidebar__heading">PRO Version</li>
+                <li>
+                    <a href="https://dashboardpack.com/theme-details/architectui-dashboard-html-pro/" target="_blank">
+                        <i class="metismenu-icon pe-7s-graph2">
+                        </i>
+                        Upgrade to PRO
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
